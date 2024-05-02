400bea817f0c8157c2996df60b9583c724cb611d
mohamed-sounkor <66226426+mohamed-sounkor@users.noreply.github.com>
2024-05-02 19:58:48 +0300

    Update userRoutes.js --adding delete user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 // Defining a route to handle user registration
 // Whenever there is a request includes (/register) then the middelware userController(registerUser function inside it) will be executed 
 router.post('/register', userController.registerUser);
-
 router.get('/', userController.getAllStudents);
+router.delete('/', userController.deleteuser);
 
 module.exports = router;
