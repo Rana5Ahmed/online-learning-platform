0b827af871034015c9b18f3eae0c6bddfec0be0d
mohamed-sounkor <muhammed195824@feng.bu.edu.eg>
2024-05-02 20:42:31 +0300

    adding delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,11 @@
 const {users} = require('../models/User');
 const { userRegistrationSchema }  = require('../helpers/vaildation')
 const Joi = require('joi');
+const { courses } = require('../models/Course');
+
+
+
+
 
 // handle user registration
     async function registerUser(req, res) {
@@ -42,6 +47,31 @@ const Joi = require('joi');
       }
     }
 
+    async function deleteuser(req, res) {
+      try {    
+        const { user_Id, password } = req.body; 
+      const backup_user = await users.findById(user_Id);
+  
+    if (!backup_user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    if (backup_user.password !== password) {
+      return res.status(400).json({ message: 'Incorrect password' });
+    }
+
+    const deletedUser = await courses.updateMany(
+      { students: { $in: [user_Id] } },
+      { $pull: { students: user_Id } },
+    );
+    
+      res.status(200).json({ message: 'User deleted successfully', deletedUser });
+    }
+       catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error', error: error.message });
+      }
+    }
+
 async function getAllStudents(req, res) {
   try {
     // Fetch all users from the database
@@ -54,6 +84,15 @@ async function getAllStudents(req, res) {
 module.exports = {
   registerUser,
   getAllStudents,
+  deleteuser,
   // To be used in courseController to check in enrollment function 
   users,
 };
+
+
+
+    // const user = await users.deleteOne({ _id: req.body.user_Id }); 
+          // const updatedCourses = await courses.updateMany(
+            // { students: user._id }, // Find courses where the user's ID exists in 'students' array
+            // { isDeleted: true }  // Remove the user's ID from 'students' array
+          // );
